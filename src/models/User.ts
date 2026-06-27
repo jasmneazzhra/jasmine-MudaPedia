d2b739a6bb37c31f8b457bb2cffac71a6070364b
jasmneazzhra <jasminezahraaa15@gmail.com>
2026-06-27 15:24:55 +0700

    feat: create user model and connect MongoDB

---
+++ src/models/User.ts
@@ -0,0 +1,36 @@
+import mongoose, { Schema, models } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+    },
+
+    image: {
+      type: String,
+      default: "",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = models.User || mongoose.model("User", userSchema);
+
+export default User;
